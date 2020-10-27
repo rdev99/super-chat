6159f7e7f97345330dc858e9fefd50efd839c33c
rdev <deveshranjan920@gmail.com>
2020-10-27 18:06:51 +0530

    started socket.io connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const http = require('http');
 const path  = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const socketio = require('socket.io');
+
 
 const app=express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,6 +25,11 @@ app.post("/chat",(req,res) => {
     res.sendFile(path.join(__dirname,'./public/chat.html'));
 })
 
+io.on('connection',(socket) => {
+    console.log('New socketio connection');
+});
+
+
 const PORT = 5000 || process.env.PORT;
 
-app.listen(PORT , () => console.log('Server started on port ' + PORT));
+server.listen(PORT , () => console.log('Server started on port ' + PORT));
